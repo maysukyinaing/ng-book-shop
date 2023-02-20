f71ed64a7ed334a05c3d597ccce6a0b17a78f545
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-02-20 15:56:55 +0630

    update(book-delete)

--- src/app/admin/all-books/all-books.component.ts
+++ src/app/admin/all-books/all-books.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import {BookService} from "../../core/service/book.service";
 import {Observable} from "rxjs";
 import {Book} from "../../core/model/book";
-import {RouterModule} from "@angular/router";
+import {Router, RouterModule} from "@angular/router";
 
 @Component({
   selector: 'app-all-books',
@@ -16,14 +16,19 @@ export class AllBooksComponent implements OnInit{
 
   books$:Observable<Book[]> = this.bookService.books$
 
-  constructor(private bookService:BookService) {
+  constructor(private bookService:BookService, private router:Router) {
   }
 
   ngOnInit(): void {
   }
 
   deleteBook(id:number | undefined) {
-    console.log(id)
+    this.bookService.deleteBook(id as number)
+      .subscribe(
+        data => console.log(data),
+        error => console.log(error),
+        () => this.router.navigate(['/list-book'])
+      )
   }
 
 
