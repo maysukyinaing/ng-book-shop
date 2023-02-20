845f5c5b1f0a7f9beeb86171e9b0a6bc4594c61f
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-02-20 10:14:02 +0630

    update(cart-service)

--- src/app/core/service/cart.service.ts
+++ src/app/core/service/cart.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable, of, retry, tap} from "rxjs";
+import {Book} from "../model/book";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+  private cartSubject:BehaviorSubject<Book[]> = new BehaviorSubject<Book[]>([])
+  carts$:Observable<Book[]> =this.cartSubject.asObservable();
+
+  private cartSizeSubject:BehaviorSubject<number>=new BehaviorSubject<number>(0)
+  cartSize$:Observable<number>=this.cartSizeSubject.asObservable();
+
+  booksCart:Book[]=[]
+
+  constructor() {
+  }
+
+  addToCart(book:Book):Observable<Book> {
+    if(!this.bookDuplicate(book)){
+      this.booksCart.push(book)
+    }
+    return of<Book>(book)
+      .pipe(
+        tap( () => this.cartSubject.next(this.booksCart)),
+        tap(() => this.cartSizeSubject.next(this.booksCart.length))  //emit last value
+      )
+  }
+
+  bookDuplicate(book:Book):boolean {
+    let isDuplicate=false
+    this.booksCart.forEach(value => {
+      if(value?.id == book.id) {
+        isDuplicate=true
+      }
+    })
+    return isDuplicate
+  }
 }
